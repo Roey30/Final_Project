310a71280b14b2ad897cd5e5f0534075eb496021
Roey30 <113986181+Roey30@users.noreply.github.com>
2023-03-08 23:30:36 +0200

    Add files via upload

---
+++ DataBase.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+username_password_storage = sqlite3.connect("username_password_storage.db")
+
+user = username_password_storage.cursor()
+
+user.execute("""CREATE TABLE IF NOT EXISTS username_password_storage (
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             name TEXT,
+             password TEXT
+             )""")
+
+username_password_storage.commit()
+username_password_storage.close()
